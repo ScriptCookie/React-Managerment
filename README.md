7d647ae5e409b5b3f3e1622db93be9325a977972
Jeung Bin kim <eoehdektnv12@gmail.com>
2022-04-14 12:01:44 +0900

    Create Customer.js

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Managerment System
+
+고객관리 시스템.
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,13 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 import React, {Component} from 'react';
+import Customer from './components/Customer';
+
+const customers = 
+[
+{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '김정빈',
+  'birthday' : '19970703',
+  'gender' : '남자',
+  'job' : '직장인'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '홍길동',
+  'birthday' : '19980502',
+  'gender' : '남자',
+  'job' : '의적'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '다빈치',
+  'birthday' : '19985413',
+  'gender' : '여자',
+  'job' : '디자이너'
+}
+
+]
 
 class App extends Component {
   render() {
     return(
-      <div className='gray_background'>
-        <img src={logo} lat='logo' />
-        <h2>Develop System!!</h2>
+      <div>
+        {
+        customers.map(c => {
+          return(
+            <Customer 
+            key = {c.id}
+            id = {c.id}
+            image = {c.image}
+            name = {c.name}
+            birthday = {c.birthday}
+            gernder = {c.gender}
+            job = {c.job}
+            />
+          );
+        })
+      }
       </div>
     ); 
   }
